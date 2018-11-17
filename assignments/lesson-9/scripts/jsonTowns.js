85ce79f8dc3b9bf0c8e105c2a80572562bc81caf
Parkinson-Kristen <35186280+Parkinson-Kristen@users.noreply.github.com>
2018-11-17 09:28:39 -0800

    e

--- assignments/lesson-9/scripts/jsonTowns.js
+++ assignments/lesson-9/scripts/jsonTowns.js
@@ -6,33 +6,30 @@ var request = new XMLHttpRequest();
     request.send();
         
     request.onload = function() {
-    var townInfo = request.response;
-    showTownInfo(townInfo);
+    var townIntro = request.response;
+    showTownIntro(townIntro);
         }
         
-    function showTownInfo(jsonObj) {
-        var town = jsonObj['towns'];
-        for (var i = 0; i < town.length; i++) {
-            
+    function showTownIntro(jsonObj) {
+        var intro = jsonObj['towns'];
+        for (var i = 0; i < intro.length; i++) {
             if (i === 2) {
-                continue;
-            }
+                continue;}
+				
             var myArticle = document.createElement('article');
             var myH2 = document.createElement('h2');
             var myH3 = document.createElement('h3');
 			var myImage = document.getElementById('myImg').src;
             var myPara1 = document.createElement('p');
             var myPara2 = document.createElement('p');
-            var myPara3 = document.createElement('p');
-            var myPara4 = document.createElement('p');
-            var myList = document.createElement('ul');
+            var myPara3 = document.createElement('p')
 
-            myH2.textContent = town[i].name;
-			myImage.textContent = town[i].imgPath;
-            myH3.textContent = 'Motto: ' + town[i].motto;
-            myPara1.textContent = 'Year Founded: ' + town[i].yearFounded;
-            myPara2.textContent = 'Population: ' + town[i].currentPopulation;
-            myPara3.textContent = 'Average Rainfall: ' + town[i].averageRainfall;
+            myH2.textContent = intro[i].name;
+			myImage.textContent = intro[i].imgPath;
+            myH3.textContent = 'Motto: ' + intro[i].motto;
+            myPara1.textContent = 'Year Founded: ' + intro[i].yearFounded;
+            myPara2.textContent = 'Population: ' + intro[i].currentPopulation;
+            myPara3.textContent = 'Average Rainfall: ' + intro[i].averageRainfall;
 
             myArticle.appendChild(myH2);
 			myArticle.appendChild(myImg);
