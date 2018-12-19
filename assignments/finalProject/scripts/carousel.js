b083afa9ec178ed1701318c94a13230e4bbbdd56
Parkinson-Kristen <35186280+Parkinson-Kristen@users.noreply.github.com>
2018-12-18 17:00:03 -0800

    H

--- assignments/finalProject/scripts/carousel.js
+++ assignments/finalProject/scripts/carousel.js
@@ -1,12 +1,28 @@
+var slideIndex = 1;
+showSlides(slideIndex);
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, options);
-  });
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-  // Or with jQuery
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-  $(document).ready(function(){
-    $('.carousel').carousel();
-  });
-      
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
